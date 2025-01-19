1285f52bbb8e4a3ab1a947142183b8f2b716b5c0
adaptedjose <josealvarezlatorre7@gmail.com>
2025-01-19 01:38:12 -0500

    Conexión con json-server | Despliegue información habitaciones

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(irSiguiente, 3000); // Cambiar de imagen cada 3 segundos
   });
 
+  // Modal functions
   function openModal() {
     document.getElementById('modal').classList.remove('hidden');
   }
@@ -37,3 +38,44 @@ document.addEventListener('DOMContentLoaded', () => {
   function closeModal() {
     document.getElementById('modal').classList.add('hidden');
   }
+
+//Function displaying rooms
+function roomCards(elemHTML, rooms) {
+    let numberRooms = "";
+
+    let availabilityText = rooms.availability ? "Ocupada" : "Disponible";
+
+    for (const room of rooms) {
+        numberRooms += `
+        <div class="room-card p-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transform transition-all">
+            <h2 class="room-title text-xl font-semibold text-gray-800">Habitación ${room.number}</h2>
+            <h3 class="room-type text-lg text-gray-600">Tipo: ${room.type}</h3>
+            <p class="room-status text-md text-gray-500">Estado: ${availabilityText}</p>
+            <img class="room-image mt-4 rounded-lg w-full h-auto" src="${room.image}" alt="Imagen de la habitación">
+        </div>`;
+    }
+
+    console.log(numberRooms);
+    elemHTML.innerHTML = numberRooms;
+}
+
+// json-server information port
+function mostrarJson(elemHTML) {
+    console.log("Cargando los datos desde json-server");
+
+    // Realizamos la solicitud al servidor
+    fetch("http://localhost:3000/rooms") // URL de tu json-server
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+            console.log(datos);
+            roomCards(elemHTML, datos);
+        })
+        .catch((error) =>
+            console.error("Error en la solicitud", error.message)
+        )
+        .finally(() => console.log("Se han cargado los datos"));
+}
+
+const final = document.getElementById("roomsDisplay");
+mostrarJson(final);
+
